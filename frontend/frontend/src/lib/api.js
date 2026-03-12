d8dbae06f5396cd57559709342bb956c6a104ec1
diptanil98 <diptanilsarkar49@gmail.com>
2026-03-12 18:23:58 +0530

    fixed the bugs

--- frontend/frontend/src/lib/api.js
+++ frontend/frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://todo-app-qrta.onrender.com/api";
 
 export function getToken() {
   return localStorage.getItem("token");
